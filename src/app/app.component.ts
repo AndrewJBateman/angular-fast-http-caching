2862f142559bc7d952e1d36daf38ce3be12d2468
AndrewJBateman <gomezbateman@yahoo.com>
2022-02-16 12:24:31 +0100

    refactor: move http call from constructor to onit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 import { map, startWith } from "rxjs/operators";
@@ -10,16 +10,22 @@ const CACHE_KEY = "httpRepoCache";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "angular-fast-http-caching";
   repos: any;
 
-  constructor(http: HttpClient) {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getRepos();
+  }
+
+  getRepos() {
     const path = "https://api.github.com/search/repositories?q=angular";
-    this.repos = http.get<any>(path).pipe(map((data: any) => data.items));
+    this.repos = this.http.get<any>(path).pipe(map((data: any) => data.items));
 
     this.repos.subscribe((next: any) => {
-      console.log('next data: ', next, typeof(next)); // type=object
+      console.log("next data: ", next, typeof next); // type=object
       localStorage[CACHE_KEY] = JSON.stringify(next); // store in local storage
     });
 
